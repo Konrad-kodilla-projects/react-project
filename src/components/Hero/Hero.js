3ed083f2b2bfc4414b7c4bfcc2bf9b40c55284b5
Konrad <konrad.bonder@gmail.com>
2019-11-12 15:07:42 +0100

    Complete 11.5

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
+
 import styles from './Hero.scss';
 import PropTypes from 'prop-types';
 
 const Hero = props => (
     <header className={styles.component}>
-        <h2 className={styles.title}>{props.titleText}</h2>
+        <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
         <img
             className={styles.image}
             src={props.img}
