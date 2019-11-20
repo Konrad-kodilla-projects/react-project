a198403dcf5b151c575f9f2c2659c244795d25c3
Konrad <konrad.bonder@gmail.com>
2019-11-20 12:52:32 +0100

    Complete 13.1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 import styles from './App.scss';
 import List from '../List/ListContainer';
 import Search from '../Search/SearchContainer';
+import Container from '../Container/Container';
 
 class App extends React.Component {
   static propTypes = {
     title: PropTypes.node,
     subtitle: PropTypes.node,
     lists: PropTypes.array,
-  }
+  };
 
   render() {
     const { title, subtitle, lists } = this.props;
@@ -18,10 +19,12 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        <Search />
-        {lists.map(listData => (
-          <List key={listData.id} {...listData} />
-        ))}
+        <Container>
+          <Search />
+          {lists.map(listData => (
+            <List key={listData.id} {...listData} />
+          ))}
+        </Container>
       </main>
     );
   }
